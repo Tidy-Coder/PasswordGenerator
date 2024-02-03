66f59d76f3e8adc7b7eb716f2ee1e64ee547c815
TidyCoder <157531579+Tidy-Coder@users.noreply.github.com>
2024-02-03 20:29:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import time
 def genereRandomNumber(tidyNumberA, tidyNumberB):
   myNumber = 1
   while True:
-    myNumber += int(str(time.time())[-1])
+    myNumber += int(str(time.time())[-1]) * int(str(time.time())[-2:])
     yield myNumber
