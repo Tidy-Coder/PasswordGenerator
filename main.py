5ae8b2535abe3418feabd21990fb95c465b1f981
TidyCoder <157531579+Tidy-Coder@users.noreply.github.com>
2024-02-03 18:26:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ def genereRandomNumber(tidyNumberA, tidyNumberB, exitNowNow = False):
     if exitNowNow == True:
       break
     if int(myNumber) % 2 == 1:
-      myNumber = str(int(myNumber + "5" + str(int(myNumber * 5))) + 453)
+      myNumber = str(int(myNumber + "5" + str(int(int(myNumber) * 5))) + 453)
     elif int(myNumber) % 5 == 3:
-      myNumber = str(int(myNumber + "5" + str(int(myNumber * 5))) + 259)
+      myNumber = str(int(myNumber + "5" + str(int(int(myNumber) * 5))) + 259)
     else:
-      myNumber = str(int(myNumber + "5" + str(int(myNumber * 5))) + 347)
+      myNumber = str(int(myNumber + "5" + str(int(int(myNumber) * 5))) + 347)
     itsA = None
     while int(myNumber) > tidyNumberB or int(myNumber) < tidyNumberA:
       if int(myNumber) > tidyNumberB:
