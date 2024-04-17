6e88da7e8d8b0e41288f6267c49e3ba97341d91f
TidyCoder <157531579+Tidy-Coder@users.noreply.github.com>
2024-04-17 14:40:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import secrets.choice
 # this is Python file
 def generateStrongPasswordTidy(numberOfLetters):
   strongPassword = "";
-  for(n in range(0, numberOfLetters+1)):
+  for n in range(0, numberOfLetters+1):
     strongPassword += choice("abcdefghijklmnopqrstuvwxyz$%ùµ*/!.;?œ“+=1234567890}]{[|''\"\"¡/");
   return strongPassword;
 
