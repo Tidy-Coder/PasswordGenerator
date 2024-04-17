11cf558f6026deca7f85b77550a795882aee0154
TidyCoder <157531579+Tidy-Coder@users.noreply.github.com>
2024-04-17 14:47:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random as randomNumberPlease
 def generateStrongPasswordTidy(numberOfLetters):
   strongPassword = "";
   for n in range(0, numberOfLetters+1):
-    strongPassword += randomNumberPlease.choice("abcdefghijklmnopqrstuvwxyz$%ùµ*/!.;?œ“+=1234567890}]{[|''\"\"¡/")
+    strongPassword += randomNumberPlease.choice("abcdefghijklmnopqrstuvwxyz$%ùµ*/!.;?œ“+=1234567890}]{[|''\"\"¡/@à°&ABCDEFGHIJKLMNOPQRSTUVWXYZ")
   return strongPassword
 
 print(generateStrongPasswordTidy(5))
